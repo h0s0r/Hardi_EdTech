f7ed94e73582fc04cab3060a086276f5688821f0
h0s0r <hardidar.ldh@gmail.com>
2025-10-12 19:21:46 +0530

    Added Wikipedia tools and Poetry issue resolved

--- app/tools.py
+++ app/tools.py
@@ -1,8 +1,11 @@
 # Importing Required Modules/Libraries
 
+import os
 from dotenv import load_dotenv
 from langchain_community.tools.tavily_search import TavilySearchResults
-import os
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+
 
 # loading API Key's from .env using dotenv
 load_dotenv()
@@ -14,3 +17,9 @@ def get_tavily_tool():
     if not api_key:
         raise ValueError("Tavily API Key not found.")
     return TavilySearchResults(api_key=api_key)
+
+# TOOL 2 - Wikipedia Tool
+
+def get_wikipedia_tool():
+    wrapper =WikipediaAPIWrapper()
+    return WikipediaQueryRun(api_wrapper=wrapper)
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,16 +1,22 @@
-from app.tools import get_tavily_tool
+from app.tools import get_tavily_tool, get_wikipedia_tool
 
 def test_tavily_1():
 
-    print("Running Test for Tavily Search Tool")
+    print("\nRunning Test for Tavily Search Tool")
     tavily_tool = get_tavily_tool()
-
     # Running a Sample - query_1
-    query_1 = "Latest Worldwide news about Artificial Intelligence."
+    query_1 = input("Enter a Query for Tavily(Real Time Web Result) Checkup - ")
     result_1 = tavily_tool.run(query_1)
-
     print("\n Tavily Search Result")
     print(result_1)
 
+def test_wikipedia():
+    print("\nRunning Test for Wikipedia Tool")
+    wiki_tool = get_wikipedia_tool()
+    query_2 = input("Enter a Query for Wikipedia(searches Wikipedia for info related to the query) Checkup - ")
+    result_1 =wiki_tool.run(query_2)
+    print(result_1)
+
 if __name__ == "__main__":
-    test_tavily_1()
+    # test_tavily_1()
+    test_wikipedia()
